dcc440c6d2a6e8a7c7bda61d98be20bdc992bcdd
MaxxZahar <maxxzahar@rambler.ru>
2024-06-23 00:15:28 +0300

    bidding box

--- client/js/openBidLearn.js
+++ client/js/openBidLearn.js
@@ -1,8 +1,19 @@
 const newHandButton = document.querySelector('.new-hand-button');
 const handC = document.querySelector('.hand');
 const biddingBox = document.querySelector('.bidding-box');
+const screen = document.querySelector('.screen');
+
+createBiddingBox();
+
+biddingBox.addEventListener('click', (e) => {
+    if (e.target.classList.contains('bid')) {
+        screen.textContent = e.target.textContent;
+        currentBid = e.target.id;
+    }
+});
 
 let currentHand;
+let currentBid;
 
 newHandButton.addEventListener('click', (e) => {
     e.preventDefault();
@@ -10,10 +21,10 @@ newHandButton.addEventListener('click', (e) => {
 });
 
 async function getHand() {
+    currentBid = undefined;
     fetch('openBidLearn/hand.json')
         .then(res => res.json())
-        .then(hand => displayHand(hand))
-        .then(hand => currentHand = hand)
+        .then(hand => { displayHand(hand); currentHand = hand; })
         .catch(err => console.log(err));
 }
 
@@ -50,5 +61,45 @@ function createCards() {
         const cardC = document.createElement('div');
         cardC.classList.add('card');
         handC.appendChild(cardC);
+    }
+}
+
+function createBiddingBox() {
+    for (let i = 0; i < 38; i++) {
+        const bidC = document.createElement('div');
+        bidC.classList.add('bid');
+        biddingBox.appendChild(bidC);
+        let bid = Math.floor(i / 5 + 1);
+        switch (i % 5) {
+            case 0:
+                bid += '\u2663';
+                break;
+            case 1:
+                bid += '\u2666';
+                break;
+            case 2:
+                bid += '\u2665';
+                break;
+            case 3:
+                bid += '\u2660';
+                break;
+            case 4:
+                bid += 'NT';
+                break;
+        }
+        if (i === 35) {
+            bid = 'PASS';
+            bidC.classList.add('pass');
+        }
+        if (i === 36) {
+            bid = 'X';
+            bidC.classList.add('double');
+        }
+        if (i === 37) {
+            bid = 'XX';
+            bidC.classList.add('redouble');
+        }
+        bidC.textContent = bid;
+        bidC.id = i;
     }
 }
