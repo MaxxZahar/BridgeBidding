3cbe3e534d709f05adcf9bfdf102ae4eb72fae97
MaxxZahar <maxxzahar@rambler.ru>
2024-08-02 17:38:22 +0300

    refactoring

--- server/index.js
+++ server/index.js
@@ -3,15 +3,13 @@ const fs = require('fs');
 const url = require('url');
 const querystring = require('querystring');
 const Deck = require('./deck');
-const utils = require('./utils/countHandParams');
 const funcs = require('./utils/funcs');
-const PAGE_OPTIONS = require('./options/options');
 
 const server = http.createServer((req, res) => {
     if (req.url === '/openBidLearn' && req.method === 'GET') {
         console.log('GET request');
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        const rStream = fs.createReadStream('../client/html/openBidLearn.html').pipe(res);
+        fs.createReadStream('../client/html/openBidLearn.html').pipe(res);
     } else if (req.url === '/sendData') {
         let body = [];
         req.on('data', chunk => {
@@ -50,33 +48,7 @@ const server = http.createServer((req, res) => {
         if (isNaN(pageNumber) || pageNumber < 1) {
             pageNumber = 1;
         }
-        fs.readFile('./data/openingBidData.csv', { encoding: 'utf-8' }, function (err, data) {
-            if (err) console.log(err.message);
-            const deals = data.split('\r\n');
-            const totalPages = Math.ceil(deals.length / PAGE_OPTIONS.DEALS_PER_PAGE);
-            if (pageNumber > totalPages) {
-                funcs.pageDoesNotExist(res);
-                console.log('PAGE DOES NOT EXIST');
-            } else {
-                const startIndex = PAGE_OPTIONS.DEALS_PER_PAGE * (pageNumber - 1);
-                const lastIndex = startIndex + PAGE_OPTIONS.DEALS_PER_PAGE;
-                let cut = deals.slice(startIndex, lastIndex);
-                cut = cut.map(function (deal, i) {
-                    deal = deal.split(';');
-                    const dealObject = {
-                        page: pageNumber,
-                        id: startIndex + i + 1,
-                        hcp: utils.countHCP(deal),
-                        form: utils.countForm(deal),
-                        vulnerability: utils.getVulnerability(deal),
-                        bid: utils.getBid(deal)
-                    }
-                    return dealObject;
-                })
-                res.writeHead(200, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify(cut));
-            }
-        });
+        funcs.getPageData(pageNumber, res);
     } else {
         funcs.pageDoesNotExist(res);
     }
--- server/utils/funcs.js
+++ server/utils/funcs.js
@@ -1,6 +1,40 @@
+const fs = require('fs');
+const utils = require('./countHandParams');
+const PAGE_OPTIONS = require('../options/options');
+
 function pageDoesNotExist(res) {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Page does not exist');
 }
 
-module.exports = { pageDoesNotExist };
+async function getPageData(pageNumber, res) {
+    fs.readFile('./data/openingBidData.csv', { encoding: 'utf-8' }, function (err, data) {
+        if (err) console.log(err.message);
+        const deals = data.split('\r\n');
+        const totalPages = Math.ceil(deals.length / PAGE_OPTIONS.DEALS_PER_PAGE);
+        if (pageNumber > totalPages) {
+            funcs.pageDoesNotExist(res);
+            console.log('PAGE DOES NOT EXIST');
+        } else {
+            const startIndex = PAGE_OPTIONS.DEALS_PER_PAGE * (pageNumber - 1);
+            const lastIndex = startIndex + PAGE_OPTIONS.DEALS_PER_PAGE;
+            let cut = deals.slice(startIndex, lastIndex);
+            cut = cut.map(function (deal, i) {
+                deal = deal.split(';');
+                const dealObject = {
+                    page: pageNumber,
+                    id: startIndex + i + 1,
+                    hcp: utils.countHCP(deal),
+                    form: utils.countForm(deal),
+                    vulnerability: utils.getVulnerability(deal),
+                    bid: utils.getBid(deal)
+                }
+                return dealObject;
+            })
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(cut));
+        }
+    });
+}
+
+module.exports = { pageDoesNotExist, getPageData };
